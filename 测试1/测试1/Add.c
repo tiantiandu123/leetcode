4f0aa2b992517bae5d39d81895874efc0961198c
tiantiandu123 <38609418+tiantiandu123@users.noreply.github.com>
2018-07-05 00:48:00 +0800

    Add files via upload

---
+++ 测试1/测试1/Add.c
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+int global = 0;
+int Add(int a, int b)
+{
+	return a + b;
+}
---
+++ 测试1/测试1/test.c
@@ -0,0 +1,9 @@
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+extern int Add(int a, int b);
+int main()
+{
+	 printf("%d\n",Add(1, 2));
+	 getchar();
+}
